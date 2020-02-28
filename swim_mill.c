8ec0c7857f8a71541cc154a6f1aa045969a8d744
Rv-ben <Rubenbramasco@gmail.com>
2020-02-27 22:48:33 -0800

    getId works

--- swim_mill.c
+++ swim_mill.c
@@ -3,24 +3,19 @@
 #include <sys/shm.h> 
 #include <stdio.h> 
 
+#define SHM_KEY 0x1234
+
+int sharedSpace[10][10];
+
 int main() 
 { 
-	// ftok to generate unique key 
-	key_t key = ftok("shmfile",65); 
-
-	// shmget returns an identifier in shmid 
-	int shmid = shmget(key,1024,0666|IPC_CREAT); 
-
-	// shmat to attach to shared memory 
-	char *str = (char*) shmat(shmid,(void*)0,0); 
 
 
-	printf("Data written in memory: %s\n",str); 
+	int shmid = shmget(SHM_KEY,sizeof(sharedSpace),IPC_CREAT); 
 
-    
+	int * shmp = shmat(shmid,NULL,0);
+
 	
-	//detach from shared memory 
-	shmdt(str); 
 
 	return 0; 
 } 
