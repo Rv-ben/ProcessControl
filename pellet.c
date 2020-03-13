593cd9da59fa619474ff8bbc86180cd44189fe3b
Rv-ben <rubenbramasco@gmail.com>
2020-03-12 23:36:59 -0700

    Added working pellet dropping

--- pellet.c
+++ pellet.c
@@ -1,10 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <time.h>
 
-#define RAND_MAX 10
+int pellet_pos_x , pellet_pos_y;
+
+char shared [10][10];
+char c;
+int shmid;
+key_t key = 5679;
+char (*stream)[10], *s;
+
+void die(char *s)
+{
+    perror(s);
+    exit(1);
+}
+
+void connect(){
+    //get shared mem id
+    if ((shmid = shmget(key,sizeof(shared), IPC_CREAT | 0666)) < 0)
+        die("shmget");
+
+    //attach to the shared memory 
+    if ((stream = shmat(shmid, NULL, 0)) ==  -1)
+        die("shmat");
+}
+
+void spawnPellet(){
+    srand(time(0));
+    pellet_pos_x = rand()%9;
+    srand(time(0));
+    pellet_pos_y = rand()%9;
+
+    stream[pellet_pos_y][pellet_pos_x] = 'o';
+}
+
+void drop(){
+    while (pellet_pos_y != 9 )
+    {   
+        stream[pellet_pos_y][pellet_pos_x] = '|';
+        pellet_pos_y++;
+        stream[pellet_pos_y][pellet_pos_x] = 'o';
+        sleep(2);
+    }
+
+    stream[pellet_pos_y][pellet_pos_x] = '|';
+    
+}
 
 
-int * p_position(){
-    int x = rand();
+int main(){
+
+    connect();
+    spawnPellet();
+    drop();
     
 }
