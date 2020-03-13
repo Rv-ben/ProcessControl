88e2f7ad0c979604d1f06ab025223b5d8a49d3fc
Rv-ben <Rubenbramasco@gmail.com>
2020-03-12 19:19:12 -0700

    Added functional movement

--- fish.c
+++ fish.c
@@ -5,17 +5,61 @@
 #include <stdlib.h>
 #define MAXSIZE     27
 
+int fishPos = 10/2;
+char (*stream)[10], *s;
+
 void die(char *s)
 {
     perror(s);
     exit(1);
 }
 
+
+void moveLeft(){
+    if(fishPos>0){
+        stream[9][fishPos] = '|';
+        fishPos--;
+        stream[9][fishPos] = '^';
+    }
+}
+
+void moveRight(){
+    if(fishPos<9){
+        stream[9][fishPos] = '|';
+        fishPos++;
+        stream[9][fishPos] = '^';
+    }
+}
+
+void spawnFish(){
+    stream[9][fishPos] = '^';
+}
+
+void sideToSide(){
+    while(1){
+
+        for(int i = fishPos; i>0; i--){
+            printf("MoveLeft");
+            moveLeft();
+            sleep(2);
+        }
+        
+        sleep(1);
+        printf("begin switch");
+
+        for(int i = 0; i<10 ;i++){
+            printf("MoveRight");
+            moveRight();
+            sleep(2);
+        }
+        
+    }
+}
+
 int main()
 {
     int shmid;
     key_t key;
-    char (*shm)[10], *s;
 
     key = 5679;
 
@@ -24,16 +68,10 @@ int main()
         die("shmget");
 
     //attach to the shared memory
-    shm = shmat(shmid, NULL, 0);
+    stream = shmat(shmid, NULL, 0);
 
-    //run 
-    int run = 1;
-
-    //keep program running
-    while(run){
-        scanf("%d",&run);
-        printf("%c",shm[0][1]);
-    }
+    spawnFish();
+    sideToSide();
 
 
     exit(0);
